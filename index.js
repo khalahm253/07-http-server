47f37d8c628aabdda2d77c3cb663c7ab1426603d
khalahm253 <khalahm253@gmail.com>
2018-05-22 23:43:03 -0700

    initial

---
+++ index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+
+const dotenv = require('dotenv').config();
+
+
+const server = require('./src/app.js');
+
+
+server.start( process.env.PORT, () => console.log(`Server up on ${process.env.PORT}`))
